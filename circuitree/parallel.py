c1b1654beade938bcb2b816ff898a02b27a7c74a
pranav-bhamidipati <pbhamidi@usc.edu>
2023-08-22 14:04:00 -0700

    slice into ttable; class to store/retrieve params

--- circuitree/parallel.py
+++ circuitree/parallel.py
@@ -1,14 +1,77 @@
 from collections import defaultdict
+from dataclasses import dataclass, field
 from pathlib import Path
-from typing import Callable, Iterable, Optional
+from typing import Any, Callable, Iterable, Optional
 import numpy as np
 import pandas as pd
 
-from .utils import DefaultMapping
+from .utils import DefaultMapping, defaultlist
 
 __all__ = [
+    "ParameterTable",
     "TranspositionTable",
 ]
+
+
+@dataclass
+class ParameterTable:
+    seeds: Iterable[int]
+    initial_conditions: Optional[defaultlist[Any]] = field(default_factory=defaultlist)
+    parameter_sets: Optional[defaultlist[Any]] = field(default_factory=defaultlist)
+
+    """Stores a table of parameter sets that are used as inputs for a stochastic game.
+    Each row includes a random seed and columns for any initial conditions and parameter 
+    values."""
+
+    def __post_init__(self):
+        n = len(self.seeds)
+        n_init = len(self.initial_conditions)
+        n_params = len(self.parameter_sets)
+        if n_init not in (0, n) or n_params not in (0, n):
+            raise ValueError(
+                "Number of initial conditions and parameters must be zero or equal to "
+                "the number of random seeds"
+            )
+
+    def __len__(self):
+        return len(self.seeds)
+
+    def __getitem__(self, index):
+        return (
+            self.seeds[index],
+            self.initial_conditions.getdefault(index, None),
+            self.parameter_sets.getdefault(index, None),
+        )
+
+    def __slice__(self, start, stop, step):
+        return (
+            self.seeds[start:stop:step],
+            self.initial_conditions[start:stop:step],
+            self.parameter_sets[start:stop:step],
+        )
+
+    @classmethod
+    def from_dataframe(
+        cls,
+        df: pd.DataFrame,
+        seed_col: str = "seed",
+        param_cols: Optional[Iterable[str]] = None,
+        init_cols: Optional[Iterable[str]] = None,
+    ):
+        if df.index.name == seed_col:
+            df.reset_index(inplace=True)
+        elif seed_col not in df.columns:
+            raise ValueError(
+                f"Dataframe does not have a column or index named '{seed_col}'."
+            )
+
+        kw = {}
+        if init_cols:
+            kw["initial_conditions"] = defaultlist(df[init_cols].values)
+        if param_cols:
+            kw["parameter_sets"] = defaultlist(df[param_cols].values)
+
+        return cls(df[seed_col].tolist(), **kw)
 
 
 class TranspositionTable:
@@ -49,33 +112,56 @@ class TranspositionTable:
     def table(self):
         return self._table
 
-    def __getitem__(self, state_wwo_visit):
+    def __getitem__(self, state_wwo_visits):
         """Access the results of visit(s) to a state. If no visit number is specified,
         return the list of all visit results."""
-        match state_wwo_visit:
-            case (state, visit):
+        match state_wwo_visits:
+            case state, slice(start=start, stop=stop, step=step):
+                return self.table[state][start:stop:step]
+            case state, [*visits]:
+                state_table = self.table[state]
+                return [state_table[visit] for visit in visits]
+            case state, visit:
                 return self.table[state][visit]
             case state:
                 return self.table[state]
 
-    def __missing__(self, state_wwo_visit):
-        """If the state is not in the table, create a new entry. If the visit number
-        is not in the registry for that state, raise an IndexError."""
-        match state_wwo_visit:
+    def __missing__(self, state_wwo_visits):
+        """If the state is not in the table, raise an error. If accessed without
+        visit number(s), create an empty entry for this state."""
+        n_visits = len(self.table[state])
+        match state_wwo_visits:
+            case state, slice(start=start, stop=stop, step=step):
+                raise IndexError(
+                    f"Cannot index slice({start}, {stop}, {step}) for state {state} "
+                    f"with {n_visits} total visits."
+                )
+            case state, [*visits]:
+                try:
+                    for v in visits:
+                        _ = self[state, v]
+                except IndexError:
+                    raise IndexError(
+                        f"Visit index {v} does not exist for state {state} with "
+                        f"{n_visits} total visits."
+                    )
             case (state, visit):
-                n_visits = len(self.table[state])
                 raise IndexError(
                     f"Visit index {visit} does not exist for state {state} with "
                     f"{n_visits} total visits."
                 )
             case state:
-                self.table[state] = self._default_factory()
-                return self.table[state]
+                return self.table[state]  # defaultdict will create a new entry
 
     def __contains__(self, state_wwo_visit):
+        n_visits = len(self.table[state])
         match state_wwo_visit:
+            case state, slice(start=start, stop=stop, step=step):
+                return state in self.table and stop <= n_visits
+            case state, [*visits]:
+                return state in self.table and all(v < n_visits for v in visits)
             case (state, visit):
-                return state in self.table and visit < len(self.table[state])
+                return state in self.table and visit < n_visits
             case state:
                 return state in self.table
 
