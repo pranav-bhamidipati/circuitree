0d39024ec9c855d59dd5baaf2af12d1a05bcaba5
pranav-bhamidipati <pbhamidi@usc.edu>
2023-09-18 17:47:35 -0700

    rename mixin for state representation to "grammar"

--- circuitree/models.py
+++ circuitree/models.py
@@ -10,9 +10,38 @@ __all__ = ["SimpleNetworkTree", "DimerNetworkTree"]
 """Classes for modeling different types of circuits."""
 
 
-class SimpleNetworkMixin:
-    """A mixin class for simple network circuits. Can be combined with the CircuiTree or
-    MultithreadedCircuiTree base classes."""
+class SimpleNetworkGrammar:
+    """A class implementing the grammar for simple network circuits. Can be combined with
+    a CircuiTree or MultithreadedCircuiTree class that implements the get_reward() method
+    ."""
+
+    def __init__(
+        self,
+        components: Iterable[Iterable[str]],
+        interactions: Iterable[str],
+        max_interactions: Optional[int] = None,
+        **kwargs,
+    ):
+        if len(set(c[0] for c in components)) < len(components):
+            raise ValueError("First character of each component must be unique")
+        if len(set(c[0] for c in interactions)) < len(interactions):
+            raise ValueError("First character of each interaction must be unique")
+
+        super().__init__(**kwargs)
+
+        self.components = components
+        self.component_map = {c[0]: c for c in self.components}
+        self.interactions = interactions
+        self.interaction_map = {ixn[0]: ixn for ixn in self.interactions}
+
+        if max_interactions is None:
+            self.max_interactions = len(self.components) ** 2  # all possible edges
+        else:
+            self.max_interactions = max_interactions
+
+        self._non_serializable_attrs.extend(
+            ["component_map", "interaction_map", "edge_options", "_recolor"]
+        )
 
     @cached_property
     def edge_options(self):
@@ -103,7 +132,7 @@ class SimpleNetworkMixin:
         return components, activations, inhbitions
 
 
-class SimpleNetworkTree(SimpleNetworkMixin, CircuiTree):
+class SimpleNetworkTree(SimpleNetworkGrammar, CircuiTree):
     """
     SimpleNetworkTree
     =================
@@ -124,113 +153,6 @@ class SimpleNetworkTree(SimpleNetworkMixin, CircuiTree):
         that encodes an incoherent feed-forward loop:
             ``*ABC::ABa_ACa_BCi``
     """
-
-    def __init__(
-        self,
-        components: Iterable[Iterable[str]],
-        interactions: Iterable[str],
-        max_interactions: Optional[int] = None,
-        **kwargs,
-    ):
-        if len(set(c[0] for c in components)) < len(components):
-            raise ValueError("First character of each component must be unique")
-        if len(set(c[0] for c in interactions)) < len(interactions):
-            raise ValueError("First character of each interaction must be unique")
-
-        super().__init__(**kwargs)
-
-        self.components = components
-        self.component_map = {c[0]: c for c in self.components}
-        self.interactions = interactions
-        self.interaction_map = {ixn[0]: ixn for ixn in self.interactions}
-
-        if max_interactions is None:
-            self.max_interactions = len(self.components) ** 2  # all possible edges
-        else:
-            self.max_interactions = max_interactions
-
-        self._non_serializable_attrs.extend(
-            ["component_map", "interaction_map", "edge_options", "_recolor"]
-        )
-
-    @cached_property
-    def edge_options(self):
-        return [
-            [c1[0] + c2[0] + ixn[0] for ixn in self.interactions]
-            for c1, c2 in product(self.components, self.components)
-        ]
-
-    def get_actions(self, genotype: str) -> Iterable[str]:
-        if self.is_terminal(genotype):
-            return list()
-
-        # Terminating assembly is always an option
-        actions = ["*terminate*"]
-
-        components_joined, interactions_joined = genotype.strip("*").split("::")
-        components = set(components_joined)
-        interactions = set(ixn[:2] for ixn in interactions_joined.split("_") if ixn)
-        if len(interactions) >= self.max_interactions:
-            return actions
-        actions.extend(list(set(c[0] for c in self.components) - components))
-        for action_group in self.edge_options:
-            if action_group:
-                c1_c2 = action_group[0][:2]
-                if set(c1_c2) <= components and c1_c2 not in interactions:
-                    actions.extend(action_group)
-
-        return actions
-
-    def do_action(self, genotype: str, action: str) -> str:
-        if action == "*terminate*":
-            new_genotype = "*" + genotype
-        else:
-            components, interactions = genotype.split("::")
-            if len(action) == 1:
-                new_genotype = "".join([components, action, "::", interactions])
-            elif len(action) == 3:
-                if interactions:
-                    delim = "_"
-                else:
-                    delim = ""
-                new_genotype = "::".join([components, interactions + delim + action])
-        return new_genotype
-
-    @staticmethod
-    def is_terminal(genotype: str) -> bool:
-        return genotype.startswith("*")
-
-    @staticmethod
-    def get_unique_state(genotype: str) -> str:
-        prefix = "*" if genotype.startswith("*") else ""
-        components, interactions = genotype.strip("*").split("::")
-        unique_components = "".join(sorted(components))
-        unique_interactions = "_".join(sorted(interactions.split("_")))
-        return prefix + unique_components + "::" + unique_interactions
-
-    @staticmethod
-    def parse_genotype(genotype: str, nonterminal_ok: bool = False):
-        if not genotype.startswith("*") and not nonterminal_ok:
-            raise ValueError(
-                f"Assembly incomplete. Genotype {genotype} is not a terminal genotype."
-            )
-        components, interaction_codes = genotype.strip("*").split("::")
-        component_indices = {c: i for i, c in enumerate(components)}
-
-        interactions = [i for i in interaction_codes.split("_") if i]
-
-        activations = []
-        inhbitions = []
-        for left, right, ixn in interactions:
-            if ixn.lower() == "a":
-                activations.append((component_indices[left], component_indices[right]))
-            elif ixn.lower() == "i":
-                inhbitions.append((component_indices[left], component_indices[right]))
-
-        activations = np.array(activations, dtype=np.int_)
-        inhbitions = np.array(inhbitions, dtype=np.int_)
-
-        return components, activations, inhbitions
 
 
 class DimersMixin:
