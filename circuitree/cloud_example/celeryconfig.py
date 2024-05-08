e48b44f3c9f18c774ce9f8a817b99f7918910d60
pranav-bhamidipati <pbhamidi@usc.edu>
2024-05-08 01:07:23 -0700

    fix naming of tasks

--- circuitree/cloud_example/celeryconfig.py
+++ circuitree/cloud_example/celeryconfig.py
@@ -1,5 +1,5 @@
 # Specify different queues for main and worker nodes
 CELERY_TASK_ROUTES = {
-    'tasks.get_reward_celery': {'queue': 'worker_node'},
-    'tasks.run_mcts_parallel': {'queue': 'main_node'},
+    "worker_tasks.get_reward_celery": {"queue": "worker_node"},
+    "main_tasks.run_mcts_parallel": {"queue": "main_node"},
 }
