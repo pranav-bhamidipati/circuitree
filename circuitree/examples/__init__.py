76e23a64028d02db097a7674e777ae03c0fb0f7f
pranav-bhamidipati <pbhamidi@usc.edu>
2024-05-07 18:43:21 -0700

    fix init file references

--- circuitree/examples/__init__.py
+++ circuitree/examples/__init__.py
@@ -1,4 +1,4 @@
 from .sequential import get_bistability_reward, BistabilityTree
 from .app import app, get_reward_celery
 from .distributed import DistributedBistabilityTree
-from .main_tasks import run_bistability_search_parallel
+from .main_tasks import run_mcts_parallel, run_bistability_search_on_cloud
